f414750e974eeff302a60aadbc3e2fc857dc672a
esconstantin2 <164790829+esconstantin2@users.noreply.github.com>
2024-06-22 12:04:35 +0200

    docker version

--- obj/Debug/net8.0/APL2003.AssemblyInfo.cs
+++ obj/Debug/net8.0/APL2003.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APL2003")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+868c2823098337a8a10da6d9d9004bb9d97f72fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("APL2003")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APL2003")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
